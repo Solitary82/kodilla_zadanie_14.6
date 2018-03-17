2a3cb21ceeb0ae248e7a8fb3b0d30f49a98eb734
Marta Waplak <mwaplak@gmail.com>
2018-03-17 19:48:22 +0100

    Edit counter.js

--- counter.js
+++ counter.js
@@ -54,7 +54,10 @@ var Counter = React.createClass({
     }  
 });
 
-var element = React.createElement(Counter);
-ReactDOM.render(element, document.getElementById('counter_two'));
-ReactDOM.render(element, document.getElementById('counter_one'));
+var element = React.createElement('div', {},
+    React.createElement(Counter),
+    React.createElement(Counter)
+);
+
+ReactDOM.render(element, document.getElementById('app'));
 
